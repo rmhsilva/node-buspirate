8342230ddd82c4ea7db3ec8ce0ddadb51f81f3a9
Ricardo da Silva <rmh.pub@me.com>
2013-08-17 23:10:05 +0100

    Change tabs to spaces

--- index.js
+++ index.js
@@ -8,14 +8,14 @@
  */
 
 var SerialPort = require('serialport').SerialPort,
-	asyncblock   = require('asyncblock'),
-	colors       = require('colors'),
-	util         = require('util'),
-	Buffer       = require('buffer').Buffer,
-	events       = require('events');
+    asyncblock = require('asyncblock'),
+    colors     = require('colors'),
+    util       = require('util'),
+    Buffer     = require('buffer').Buffer,
+    events     = require('events');
 
 var Uart = require('./lib/uart'),
-	Spi  = require('./lib/spi');
+    Spi  = require('./lib/spi');
 
 module.exports = BusPirate;
 
@@ -27,68 +27,68 @@ module.exports = BusPirate;
  * @param {bool} debug Debug mode flag, default false
  */
 function BusPirate(device, baud, debug) {
-	events.EventEmitter.call(this);
-	var self = this;
-	this.debug = debug || false;
-	this.waiters = [];
+  events.EventEmitter.call(this);
+  var self = this;
+  this.debug = debug || false;
+  this.waiters = [];
 
-	baud = baud || 115200;
-	this.log('info', 'Initialising BusPirate at '+device);
-	this.port = new SerialPort(device, { baudrate: baud });
+  baud = baud || 115200;
+  this.log('info', 'Initialising BusPirate at '+device);
+  this.port = new SerialPort(device, { baudrate: baud });
 
-	// Modes
-	this.mode = '';
-	this.uart = new Uart(self);
-	this.spi = new Spi(self);
+  // Modes
+  this.mode = '';
+  this.uart = new Uart(self);
+  this.spi = new Spi(self);
 
-	// Once the port opens, enter binary mode (bitbang)
-	this.port.on('open', function() {
-		self.log('info', 'Device open', device);
+  // Once the port opens, enter binary mode (bitbang)
+  this.port.on('open', function() {
+    self.log('info', 'Device open', device);
 
-		// Generic error handler
-		self.port.on('error', function(err) {
-			self.log('error', err);
-			self.emit('error', err);
-		});
+    // Generic error handler
+    self.port.on('error', function(err) {
+      self.log('error', err);
+      self.emit('error', err);
+    });
 
-		// As soon as it's open, reset console and go binmode
-		self.reset_console();
-		self.enter_binmode(function(err) {
-			if (err) {
-				if(self.listeners('error').length)
-					self.emit('error', err);
-				else
-					throw new Error(err);
-			}
-			else {
-				// Drain the serial port
-				self.port.flush(function() {
-					self.emit('connected');
-				});
-			}
-		});
+    // As soon as it's open, reset console and go binmode
+    self.reset_console();
+    self.enter_binmode(function(err) {
+      if (err) {
+        if(self.listeners('error').length)
+          self.emit('error', err);
+        else
+          throw new Error(err);
+      }
+      else {
+        // Drain the serial port
+        self.port.flush(function() {
+          self.emit('connected');
+        });
+      }
+    });
 
-		// Set up handlers for data sent from the buspirate
-		self.port.on('data', function(data) {
-			self.log('receive', format(data).red);
-			self.emit('receive', data);
+    // Set up handlers for data sent from the buspirate
+    self.port.on('data', function(data) {
+      self.log('receive', format(data).red);
+      self.emit('receive', data);
 
-			// Give the received data to any waiting functions
-			if (self.waiters.length > 0) {
-				for (var i = self.waiters.length - 1; i >= 0; i--) {
-					data = self.waiters[i](data, i, self.waiters);
-				}
-			}
-		});
+      // Give the received data to any waiting functions
+      if (self.waiters.length > 0) {
+        for (var i = self.waiters.length - 1; i >= 0; i--) {
+          data = self.waiters[i](data, i, self.waiters);
+        }
+      }
+    });
 
-		// Reset to binmode when exiting via Control-C
-		process.on('SIGINT', function() {
-			console.log('EXITING. Press Control-D to force'.red);
-			self.enter_binmode(function() {
-				process.exit(0);
-			});
-		});
-	});
+    // Reset to binmode when exiting via Control-C
+    process.on('SIGINT', function() {
+      console.log('EXITING. Press Control-D to force'.red);
+      self.enter_binmode(function() {
+        process.exit(0);
+      });
+    });
+  });
 }
 
 // BusPirate is an event emitter!
@@ -99,28 +99,28 @@ util.inherits(BusPirate, events.EventEmitter);
  * Make sure we aren't in any menus or anything, and send # to reset
  */
 BusPirate.prototype.reset_console = function() {
-	this.write([0x0d, 0x0d, 0x0d, 0x0d, 0x0d,    // Enter, 10 times
-				0x0d, 0x0d, 0x0d, 0x0d, 0x0d, 0x23]);  // and then #
+  this.write([0x0d, 0x0d, 0x0d, 0x0d, 0x0d,    // Enter, 10 times
+        0x0d, 0x0d, 0x0d, 0x0d, 0x0d, 0x23]);  // and then #
 };
 
 /**
  * Enter binary mode by writing 0x00 enough times
  */
 BusPirate.prototype.enter_binmode = function(callback) {
-	// Periodically write 0x00...
-	var self = this;
-	var binmodeIntervalID = setInterval(function(self) {
-		self.write(0x00);
-	}, 20, this);
+  // Periodically write 0x00...
+  var self = this;
+  var binmodeIntervalID = setInterval(function(self) {
+    self.write(0x00);
+  }, 20, this);
 
-	// ... until BBIO1 is received
-	this.wait_for_data('BBIO1', function(err) {
-		clearInterval(binmodeIntervalID);
-		self.log('Binmode entered successfully');
-		self.mode = 'binmode';
-		self.emit('mode', 'binmode');
-		if (callback) callback();
-	});
+  // ... until BBIO1 is received
+  this.wait_for_data('BBIO1', function(err) {
+    clearInterval(binmodeIntervalID);
+    self.log('Binmode entered successfully');
+    self.mode = 'binmode';
+    self.emit('mode', 'binmode');
+    if (callback) callback();
+  });
 };
 
 /**
@@ -131,22 +131,22 @@ BusPirate.prototype.enter_binmode = function(callback) {
  * @param  {Function} callback
  */
 BusPirate.prototype.switch_mode = function(newmode, callback) {
-	var self = this;
-	if (this.mode != 'binmode') {
-		this.log('warn', 'Switching mode from '+this.mode+' to '+newmode.MODE_NAME);
-		this.reset_console();
-		this.enter_binmode();
-	}
-	else this.log('info', 'Switching to mode: '+newmode.MODE_NAME);
+  var self = this;
+  if (this.mode != 'binmode') {
+    this.log('warn', 'Switching mode from '+this.mode+' to '+newmode.MODE_NAME);
+    this.reset_console();
+    this.enter_binmode();
+  }
+  else this.log('info', 'Switching to mode: '+newmode.MODE_NAME);
 
-	this.write(newmode.MODE_ID);
-	this.wait_for_data(newmode.MODE_ACK, function(err, data) {
-		self.log('mode', newmode.MODE_NAME);
-		self.mode = newmode.MODE_NAME;
-		self.emit('mode', newmode.MODE_NAME);
+  this.write(newmode.MODE_ID);
+  this.wait_for_data(newmode.MODE_ACK, function(err, data) {
+    self.log('mode', newmode.MODE_NAME);
+    self.mode = newmode.MODE_NAME;
+    self.emit('mode', newmode.MODE_NAME);
 
-		if (callback) callback(err, newmode.MODE_NAME);
-	});
+    if (callback) callback(err, newmode.MODE_NAME);
+  });
 };
 
 /**
@@ -154,16 +154,16 @@ BusPirate.prototype.switch_mode = function(newmode, callback) {
  * @return {err}  null if everything worked fine
  */
 BusPirate.prototype.config_periph = function(power, pullups, aux, cs, callback) {
-	// TODO: make sure we're in an allowed mode?
-	var code = 0x40 + power*8 + pullups*4 + aux*2 + cs;
-	var self = this;
+  // TODO: make sure we're in an allowed mode?
+  var code = 0x40 + power*8 + pullups*4 + aux*2 + cs;
+  var self = this;
 
-	this.write(code);
-	this.wait_for_data(0x01, function(err, data) {
-		self.log('peripherals', code);
-		self.emit('peripherals', code);
-		if (callback) callback(err);
-	});
+  this.write(code);
+  this.wait_for_data(0x01, function(err, data) {
+    self.log('peripherals', code);
+    self.emit('peripherals', code);
+    if (callback) callback(err);
+  });
 };
 
 
@@ -176,11 +176,11 @@ BusPirate.prototype.config_periph = function(power, pullups, aux, cs, callback) 
  * @param  {Function} callback  Function to execute after writing
  */
 BusPirate.prototype.write = function(data, callback) {
-	this.log('write', data);
-	if (data instanceof Array || 'string' === typeof data)
-		this.port.write(data, callback);  // TODO: check num bytes written?
-	else
-		this.port.write([data], callback);
+  this.log('write', data);
+  if (data instanceof Array || 'string' === typeof data)
+    this.port.write(data, callback);  // TODO: check num bytes written?
+  else
+    this.port.write([data], callback);
 };
 
 /**
@@ -190,11 +190,11 @@ BusPirate.prototype.write = function(data, callback) {
  * @param  {string|array|number} data  Data to write
  */
 BusPirate.prototype.sync_write = function(flow, data) {
-	var self = this;
+  var self = this;
 
-	// TODO: catch errors in write and read data
-	this.write(data, flow.add());
-	flow.wait();
+  // TODO: catch errors in write and read data
+  this.write(data, flow.add());
+  flow.wait();
 };
 
 /**
@@ -203,40 +203,40 @@ BusPirate.prototype.sync_write = function(flow, data) {
  * @param  {Function} callback
  */
 BusPirate.prototype.wait_for_data = function(data, callback) {
-	var self = this;
+  var self = this;
 
-	// Convert data into a form that is easily compared with a Buffer
-	if (data instanceof Array || 'string' === typeof data)
-		data = new Buffer(data);
-	else
-		data = new Buffer([data]);
+  // Convert data into a form that is easily compared with a Buffer
+  if (data instanceof Array || 'string' === typeof data)
+    data = new Buffer(data);
+  else
+    data = new Buffer([data]);
 
-	this.log('listener', 'Added waiter for', format(data).yellow);
+  this.log('listener', 'Added waiter for', format(data).yellow);
 
-	// Add the waiter function to the start of the waiters array.  It is 
-	// iterated over backwards.  This way, the first added is the first called
-	this.waiters.unshift(function(data_received, idx, arr) {
-		self.log('listener', 'Waiting for: '+format(data).blue+' got: '+format(data_received).blue);
-		if (data_received.length < data.length)
-			return data_received;
+  // Add the waiter function to the start of the waiters array.  It is 
+  // iterated over backwards.  This way, the first added is the first called
+  this.waiters.unshift(function(data_received, idx, arr) {
+    self.log('listener', 'Waiting for: '+format(data).blue+' got: '+format(data_received).blue);
+    if (data_received.length < data.length)
+      return data_received;
 
-		// wait for '' => return the next lump of data that arrives
-		if (data.length === 0) {
-			data = data_received;
-		}
-		else {
-			for (var i = data.length - 1; i >= 0; i--) {
-				if (data[i] != data_received[i])
-					return data_received;
-			}
-		}
+    // wait for '' => return the next lump of data that arrives
+    if (data.length === 0) {
+      data = data_received;
+    }
+    else {
+      for (var i = data.length - 1; i >= 0; i--) {
+        if (data[i] != data_received[i])
+          return data_received;
+      }
+    }
 
-		// If matches, remove this waiter and the data it consumed
-		self.log('listener', 'Found '+format(data).green);
-		arr.splice(idx, 1);
-		callback(null, data);
-		return data_received.slice(data.length);
-	});
+    // If matches, remove this waiter and the data it consumed
+    self.log('listener', 'Found '+format(data).green);
+    arr.splice(idx, 1);
+    callback(null, data);
+    return data_received.slice(data.length);
+  });
 };
 
 
@@ -244,12 +244,12 @@ BusPirate.prototype.wait_for_data = function(data, callback) {
  * Synchronous version of wait, similar to sync_write, above, optional timeout
  */
 BusPirate.prototype.sync_wait = function(flow, data, timeout) {
-	flow.on('taskTimeout', function() {
-		return new Error('Timeout while waiting for:', data);
-	});
+  flow.on('taskTimeout', function() {
+    return new Error('Timeout while waiting for:', data);
+  });
 
-	this.wait_for_data(data, flow.add({timeout: timeout}));
-	flow.wait();
+  this.wait_for_data(data, flow.add({timeout: timeout}));
+  flow.wait();
 };
 
 
@@ -257,25 +257,25 @@ BusPirate.prototype.sync_wait = function(flow, data, timeout) {
  * Debug logger - log(type, message, ...)
  */
 BusPirate.prototype.log = function() {
-	var argv = [].slice.call(arguments);
+  var argv = [].slice.call(arguments);
 
-	if (this.debug) {
-		console.log('BP: '.cyan + argv.shift().green + ' ' + argv.map(format).join(' '));
-	}
+  if (this.debug) {
+    console.log('BP: '.cyan + argv.shift().green + ' ' + argv.map(format).join(' '));
+  }
 };
 
 /**
  * Formatting for the logger items.  eg numbers are shown in hex
  */
 function format(item) {
-	if (typeof item === 'number') {
-		return '0x'+item.toString(16);
-	}
-	else if (Buffer.isBuffer(item)) {
-		return item.toString('utf-8') +'['+item.toString('hex')+']';
-	}
-	else if (typeof item === 'object') {
-		return item.map(format);
-	}
-	else return item;
+  if (typeof item === 'number') {
+    return '0x'+item.toString(16);
+  }
+  else if (Buffer.isBuffer(item)) {
+    return item.toString('utf-8') +'['+item.toString('hex')+']';
+  }
+  else if (typeof item === 'object') {
+    return item.map(format);
+  }
+  else return item;
 }
